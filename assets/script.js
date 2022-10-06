0bc4b35f3667fe514e6d2053305c5eb9b8c25150
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-10-05 20:36:31 -0400

    added the timer function as well and onclick function to start timer and ide the main quiz header

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+var startBtn = document.querySelector('#start-btn');
+var timeEl = document.querySelector('.timer');
+var quizIntro = document.querySelector('.quiz-intro');
+
+var secondsleft = 70;
+
+
+
+// this the timer function
+function setTime() {
+    
+var timerInterval = setInterval(function() {
+      secondsleft--;
+      timeEl.textContent = secondsleft;
+  
+      if(secondsleft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+      } 
+ }, 1000);
+}
+
+// when start button is clicked it starts the timer and hides the quiz intro
+startBtn.addEventListener('click', function() {
+    quizIntro.setAttribute("style", "display: none")
+
+    setTime()
+})
+
+
