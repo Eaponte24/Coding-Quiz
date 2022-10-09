e9d0653bfda1967ac4c9474d4a19dd3aded2e26d
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-10-09 12:21:49 -0400

    added array for the questions and options and answers

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,57 @@ var quizIntro = document.querySelector('.quiz-intro');
 var quizContent = document.querySelector('.quiz-content');
 var secondsleft = 70;
 
+var quizQuestions = [
+  {
+    //index 0
+    number: 1,
+    question: "Number of primitive data types in Java are?",
+    answer: "Eight"
+    options: [
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine"
+    ]
+  }
+  {
+    //index 1
+    number: 2,
+    question: "Arrays in java are-?",
+    answer: "objects"
+    options: [
+      "Object references",
+      "Objects",
+      "Primitive data type",
+      "None"
+    ]
+  }
+  {
+    //index 2
+    number: 3,
+    question: "Which of the following is not javascript data types?",
+    answer: "All of the above"
+    options: [
+      "Null type",
+      "Undefined type",
+      "Number type",
+      "All of the above"
+    ]
+  }
+  {
+    //index 3
+    number: 4,
+    question: "Why event handlers is needed in JS?",
+    answer: "Allows JavaScript code to alter the behaviour of windows"
+    options: [
+      "Allows JavaScript code to alter the behaviour of windows",
+      "Adds innerHTML page to the code",
+      "Change the server location",
+      "Performs handling of exceptions and occurrences"
+    ]
+  }
+]
+
 
 
 // this the timer function
